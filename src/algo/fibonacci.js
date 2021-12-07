dbee8eba02fe8a4dc4862cd5cecdbd7637088924
chenhu.297 <chenhu.297@bytedance.com>
2021-12-07 15:18:09 +0800

    feat: fibonaci

---
+++ src/algo/fibonacci.js
@@ -0,0 +1,13 @@
+const map = {
+  0: 0,
+  1: 1,
+  2: 1,
+};
+var tribonacci = function (n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (n === 2) return 1;
+  return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+};
+
+console.log(fib(25));
